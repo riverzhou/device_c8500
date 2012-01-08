2fab218ba10ed7599dc431edaf04d9464c30f8b8
River Zhou <riverzhou2000@gmail.com>
2012-01-08 10:37:08 +0800

    rild works now and then

--- Android.mk
+++ Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(my-dir)
 
 ifeq ($(TARGET_DEVICE),c8500)
-    include $(call all-named-subdir-makefiles, libaudio , libcopybit ) 
+    include $(call all-named-subdir-makefiles, libaudio libcopybit ) 
 endif
 
 
