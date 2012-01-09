a80b8b6f3720a4a12eb984c107cd37d64532c790
River Zhou <riverzhou2000@gmail.com>
2012-01-09 12:18:26 +0800

    clean script

--- bao/clean.sh
+++ bao/clean.sh
@@ -3,5 +3,11 @@
 find system -name \*fish\* -delete
 rm -f system/lib/hw/gralloc.default.so
 rm -f system/app/RomManager.apk
+rm -f system/etc/AudioFilterU8150.csv
+rm -f system/etc/enhanced.conf
+rm -f system/etc/init.local.rc
+rm -f system/etc/init.d/04modules
+rm -f system/etc/init.d/20userinit
+rm -f system/etc/init.d/S97ramscript
 
 
