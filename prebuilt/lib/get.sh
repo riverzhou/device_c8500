d23973c8d6cbd3593ed94f5481b363c7451eda13
River Zhou <riverzhou2000@gmail.com>
2012-01-14 20:40:36 +0800

    add a get prebuilt file script

---
+++ prebuilt/lib/get.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+S=/river/rom
+W=/system/lib
+
+S1=${S}/s1/${W}
+S2=${S}/s2/${W}
+S3=${S}/s3/${W}
+
+f=`ls`
+
+for i in $f ; do
+	if [ -e ${S1}/${i} ] ; then
+		cp ${S1}/${i} .
+		echo ${S1}/${i}
+		continue
+	fi
+	if [ -e ${S2}/${i} ] ; then
+		cp ${S2}/${i} .
+		echo ${S2}/${i}
+		continue
+	fi
+	if [ -e ${S3}/${i} ] ; then
+		cp ${S3}/${i} .
+		echo ${S3}/${i}
+		continue
+	fi
+done
+
+
